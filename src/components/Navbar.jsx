2e413485c2f9918dbbe57264ca8324dd748d772f
iamadityamall <contact.adityamall@gmail.com>
2022-03-23 15:37:50 +0530

    resume-app

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { FiArrowLeft } from "react-icons/fi";
+import { AiFillFilePdf } from "react-icons/ai";
+import { FiDownload } from "react-icons/fi";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { useState } from "react";
+import OtherOptions from "./OtherOptions";
+
+const Navbar = () => {
+  const [menu, setMenu] = useState(false);
+
+  return (
+    <nav className="w-screen flex justify-between px-3 py-4 bg-black/95 text-white">
+      {/*navbar left section*/}
+      <div className="flex items-center space-x-8">
+        <button className="p-2">
+          <FiArrowLeft className="text-2xl" />
+        </button>
+        <div className="flex items-center space-x-4">
+          <div>
+            <AiFillFilePdf className="text-2xl text-red-600" />
+          </div>
+          <div className="text-s font-sans">Resume-Aditya-Mall.pdf</div>
+        </div>
+      </div>
+
+      {/* navbar right section */}
+      <div className="flex items-center text-xl space-x-8">
+        <button onClick={() => alert("you clicked me")}>
+          <FiDownload />
+        </button>
+        <button onClick={() => setMenu(!menu)}>
+          <BsThreeDotsVertical />
+        </button>
+      </div>
+
+      {menu ? <OtherOptions/> : null}
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/components/OtherOptions.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const OtherOptions = () => {
+  return (
+    <nav className="rounded-lg absolute right-0 top-14 z-100 text-black mr-3 bg-yellow-200 transition delay-75 ease-linear list-none">
+      <li className="p-3">Home</li>
+      <li className="p-3">Contact</li>
+      <li className="p-3">projects</li>
+    </nav>
+  );
+}
+
+export default OtherOptions
