ae32edcc06b034025938349b0b5b6ef9e8bcfa9a
iamadityamall <contact.adityamall@gmail.com>
2022-03-28 00:47:06 +0530

    edited the title using use effect

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import React from "react";
 import Navbar from "./components/Navbar";
 // import Footer from "./components/Footer";
 import MainContent from "./components/MainContent";
+import { useEffect } from "react";
 
 const App = () => {
+  useEffect(() => {
+    document.title = "Resume-Aditya-Mall"
+  }, [])
   return (
     <>
       <Navbar />
