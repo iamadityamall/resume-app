1786853dad48ffff9257e439af0b608a1bdfe5ec
iamadityamall <contact.adityamall@gmail.com>
2022-03-28 01:01:03 +0530

    added more options

--- src/components/OtherOptions.jsx
+++ src/components/OtherOptions.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const OtherOptions = () => {
   return (
-    <nav className="rounded-lg absolute right-0 top-14 z-100 text-black mr-3 bg-yellow-200 transition delay-75 ease-linear list-none">
+    <nav className=" rounded-sm fixed right-0 top-14 text-black mr-3 bg-secondary transition delay-75 ease-linear list-none w-72">
+      <li className="p-3">Home</li>
+      <li className="p-3">Contact</li>
+      <li className="p-3">projects</li>
       <li className="p-3">Home</li>
       <li className="p-3">Contact</li>
       <li className="p-3">projects</li>
