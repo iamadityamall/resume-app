0b58b452e70a8bc228e010a6e22abdd85e2d3f19
iamadityamall <contact.adityamall@gmail.com>
2022-03-29 11:47:10 +0530

    updated

--- src/components/OtherOptions.jsx
+++ src/components/OtherOptions.jsx
@@ -4,13 +4,13 @@ import { IoMdOpen } from "react-icons/io";
 
 const OtherOptions = () => {
   return (
-    <nav className=" rounded-sm fixed right-[-3px] top-14 text-black mr-3 bg-primary transition delay-75 ease-linear list-none w-52 drop-shadow-xl font-secondary">
-      <li className=" p-3 cursor-pointer flex items-center space-x-6">
-        <BiFileFind className="text-xl" />
+    <nav className=" rounded-sm fixed right-[-3px] top-14 text-black mr-3 bg-primary transition delay-75 ease-linear list-none w-60 drop-shadow-xl font-secondary">
+      <li className=" p-4 cursor-pointer flex items-center space-x-6">
+        <BiFileFind className="text-xl text-gray-600" />
         <p className="text-xs">Find</p>
       </li>
-      <li className="p-3 cursor-pointer flex items-center space-x-6">
-        <IoMdOpen className="text-xl" />
+      <li className="p-4 cursor-pointer flex items-center space-x-6">
+        <IoMdOpen className="text-xl text-gray-600" />
         <p className="text-xs">Open in new window</p>
       </li>
     </nav>
