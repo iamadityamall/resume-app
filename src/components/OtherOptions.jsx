7ff9d620dc81b9501b1981551bc5b1383f78f891
iamadityamall <contact.adityamall@gmail.com>
2022-03-29 11:44:44 +0530

    update

--- src/components/OtherOptions.jsx
+++ src/components/OtherOptions.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React from "react";
+import { BiFileFind } from "react-icons/bi";
+import { IoMdOpen } from "react-icons/io";
 
 const OtherOptions = () => {
   return (
-    <nav className=" rounded-lg fixed right-[-2px] top-14 text-black mr-3 bg-secondary transition delay-75 ease-linear list-none w-72">
-      <li className="p-3 cursor-pointer text-xl">Home</li>
-      <li className="p-3 cursor-pointer text-xl">Contact</li>
-      <li className="p-3 cursor-pointer text-xl">projects</li>
-      <li className="p-3 cursor-pointer text-xl">Home</li>
-      <li className="p-3 cursor-pointer text-xl">Contact</li>
-      <li className="p-3 cursor-pointer text-xl">projects</li>
+    <nav className=" rounded-sm fixed right-[-3px] top-14 text-black mr-3 bg-primary transition delay-75 ease-linear list-none w-52 drop-shadow-xl font-secondary">
+      <li className=" p-3 cursor-pointer flex items-center space-x-6">
+        <BiFileFind className="text-xl" />
+        <p className="text-xs">Find</p>
+      </li>
+      <li className="p-3 cursor-pointer flex items-center space-x-6">
+        <IoMdOpen className="text-xl" />
+        <p className="text-xs">Open in new window</p>
+      </li>
     </nav>
   );
-}
+};
 
-export default OtherOptions
+export default OtherOptions;
